e79e5f7f5a7ce5ff9ce73df8d2fb7f7d70cbdaf6
Javier <jmartin@grupoloyal.com>
2022-07-26 18:35:48 +0200

    OpenApi

--- demo/src/main/java/com/example/applications/resources/ActorResource.java
+++ demo/src/main/java/com/example/applications/resources/ActorResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.ApiExceptionHandler.ErrorMessage;
 import com.example.domains.contracts.services.ActorService;
 import com.example.domains.entities.Actor;
 import com.example.domains.entities.Film;
@@ -31,9 +33,20 @@ import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import org.springframework.http.HttpStatus;
 
 @RestController
+@Tag(name = "actores-service", description = "Mantenimiento de actores")
 @RequestMapping("/api/v1/actores")
 public class ActorResource {
 	@Autowired
@@ -45,12 +58,15 @@ public class ActorResource {
 	}
 
 	@GetMapping(params = "page")
-	public Page<ActorShort> getAll(Pageable page) {
+	public Page<ActorShort> getAll(@ParameterObject Pageable page) {
 		return srv.getByProjection(page, ActorShort.class);
 	}
 
 	@GetMapping(path = "/{id:\\d+}/**")
-	public ActorShort getOne(@PathVariable int id) throws NotFoundException {
+	@Operation(summary = "Buscar un actor",  description = "Devuelve un actor por su identificador" )
+	@ApiResponse(responseCode = "200", description = "Actor encontrado")
+	@ApiResponse(responseCode = "404", description = "Actor no encontrado")
+	public ActorShort getOne(@Parameter(description = "Identificador del actro", required = true) @PathVariable int id) throws NotFoundException {
 		var item = srv.getOne(id);
 		if(item.isEmpty())
 			throw new NotFoundException();
@@ -70,6 +86,9 @@ public class ActorResource {
 	
 	
 	@PostMapping
+	@Operation(summary = "Buscar un actor",  description = "Devuelve un actor por su identificador")
+	@ApiResponse(responseCode = "200", description = "Actor encontrado")
+	@ApiResponse(responseCode = "404", description = "Actor no encontrado")
 	public ResponseEntity<Object> create(@Valid @RequestBody ActorShort item) throws BadRequestException, DuplicateKeyException, InvalidDataException {
 		var newItem = ActorShort.from(item);
 		newItem = srv.add(newItem);
@@ -81,6 +100,18 @@ public class ActorResource {
 
 	@PutMapping("/{id}")
 	@ResponseStatus(HttpStatus.ACCEPTED)
+	@Operation(summary = "Modificar un actor",  description = "Sustituye un actor con los nuevos datos, los identificadores deben coincidir.",
+		tags = {"modificaciones"},
+		parameters = {
+				@Parameter(in = ParameterIn.PATH, name = "id", required = true, description = "Identificador del actor")
+		},
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Datos del actor", required = true, content = @Content(mediaType = "application/json", schema = @Schema(implementation = ActorShort.class))),
+		responses = {
+				@ApiResponse(responseCode = "202", description = "Actor modificado"),
+				@ApiResponse(responseCode = "400", description = "Datos invalidos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+				@ApiResponse(responseCode = "404", description = "Actor no encontrado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))			
+		}
+	)
 	public void update(@PathVariable int id, @Valid @RequestBody ActorShort item) throws BadRequestException, NotFoundException, InvalidDataException {
 		if(item.getActorId() != id) 
 			throw new BadRequestException("No coinciden los identificadores");
--- demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++ demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
@@ -4,8 +4,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.Length;
-
 import com.example.domains.entities.Actor;
 
 import io.swagger.v3.oas.annotations.media.Schema;
